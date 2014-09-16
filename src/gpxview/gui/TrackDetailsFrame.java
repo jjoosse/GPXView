8c6acb48f46ee91bcc1675e31ed78e52b281d197
jjoosse <jcjoosse@gmail.com>
2014-09-16 14:33:58 +0200

    Add close button.

--- src/gpxview/gui/TrackDetailsFrame.java
+++ src/gpxview/gui/TrackDetailsFrame.java
@@ -50,6 +50,7 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
                 jLabel2 = new javax.swing.JLabel();
                 jScrollPane1 = new javax.swing.JScrollPane();
                 jTable1 = new javax.swing.JTable();
+                jButton1 = new javax.swing.JButton();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -66,6 +67,13 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
                 jTable1.setModel(new TrackTableModel());
                 jScrollPane1.setViewportView(jTable1);
 
+                jButton1.setText("Close");
+                jButton1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jButton1ActionPerformed(evt);
+                        }
+                });
+
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
                 getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
@@ -73,7 +81,7 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 612, Short.MAX_VALUE)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addComponent(jLabel1)
@@ -82,7 +90,10 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                                .addGap(0, 0, Short.MAX_VALUE))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addGap(0, 0, Short.MAX_VALUE)
+                                                .addComponent(jButton1)))
                                 .addContainerGap())
                 );
                 layout.setVerticalGroup(
@@ -97,14 +108,21 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
                                         .addComponent(jLabel2)
                                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE)
-                                .addContainerGap())
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 335, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1))
                 );
 
                 pack();
         }// </editor-fold>//GEN-END:initComponents
 
+        private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+		this.setVisible(false);
+		this.dispose();                // TODO add your handling code here:
+        }//GEN-LAST:event_jButton1ActionPerformed
+
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JButton jButton1;
         private javax.swing.JLabel jLabel1;
         private javax.swing.JLabel jLabel2;
         private javax.swing.JScrollPane jScrollPane1;
@@ -115,7 +133,7 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
 
 	private final DateFormat df;
 	private final Track track1;
-
+	
 	private void fillTable() {
 		ArrayList<ArrayList<String>> d = new ArrayList<ArrayList<String>>();
 		for (TrackSegment g : track1.getTrackSegmentList()) {
@@ -131,14 +149,15 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
 		((TrackTableModel) jTable1.getModel()).setTableData(d);
 	}
 	
-	
 	class TrackTableModel extends AbstractTableModel {
+
 		ArrayList<ArrayList<String>> tableData;
 		String[] columnNames = {"Latitude", "Longitude", "Elevation", "Time"};
-		public Class<?>[]  colTypes = {String.class,
-                                      String.class,
-                                      String.class,
-                                      String.class};
+		public Class<?>[] colTypes = {String.class,
+			String.class,
+			String.class,
+			String.class};
+
 		@Override
 		public int getRowCount() {
 			if (tableData != null) {
@@ -147,12 +166,12 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
 				return 0;
 			}
 		}
-
+		
 		@Override
 		public int getColumnCount() {
 			return columnNames.length;
 		}
-
+		
 		@Override
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			return tableData.get(rowIndex).get(columnIndex);
@@ -162,18 +181,17 @@ public class TrackDetailsFrame extends javax.swing.JFrame {
 			this.tableData = data;
 			fireTableDataChanged();
 		}
-
+		
 		@Override
 		public Class<?> getColumnClass(int columnIndex) {
 			return colTypes[columnIndex];
 			
 		}
-
+		
 		@Override
 		public String getColumnName(int column) {
 			return columnNames[column];
 		}
-		
 		
 	}
 }
